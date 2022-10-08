01762eaabb7927cfbb29c87bb33572fa790a43df
unknown <kownjinwoo@gmail.com>
2022-10-08 16:40:15 +0900

    업비트 김프 추가

--- 05_01.py
+++ 05_01.py
@@ -3,25 +3,35 @@ import ccxt
 from bs4 import BeautifulSoup
 import urllib.request as req
 import re
+import pyupbit
+
+#업비트 BTC 가격
+price_upbit_btc = pyupbit.get_current_price("KRW-BTC")
 
 #바이낸스 BTC가격
 binance = ccxt.binance()
 ticker = binance.fetch_ticker('BTC/USDT')
+price_btc_usdt = int(ticker['close']) # 미국 BTC 가격
+
+#빗썸 BTC 가격
+price_bithumb_btc = int(Bithumb.get_current_price("BTC"))
 
 # 원달러 환율
 url = "http://finance.naver.com/marketindex/"
 res = req.urlopen(url)
 soup = BeautifulSoup(res, "html.parser")
-
-abc = soup.select_one("div.head_info > span.value").string #미국 원달러 환율
+abc = soup.select_one("div.head_info > span.value").string
 price_usd_krw = int(re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z\s]", "", abc)) / 100
 
-price_btc_krw = int(Bithumb.get_current_price("BTC")) # 한국 BTC 가격
 
-price_btc_usdt = int(ticker['close']) # 미국 BTC 가격
+price_btc_krw_usdt = price_usd_krw * price_btc_usdt # 바이낸스 BTC 가격 KRW로 변환
 
-price_btc_krw_usdt = price_usd_krw * price_btc_usdt
+#Bithumb 김프
+kimp_bithumb =((price_bithumb_btc - price_btc_krw_usdt) / price_btc_krw_usdt ) * 100
 
-kimp =((price_btc_krw - price_btc_krw_usdt) / price_btc_krw_usdt )*100 #kimp 가격
+#Upbit 김프
+kimp_upbit =((price_upbit_btc - price_btc_krw_usdt) / price_btc_krw_usdt ) * 100
 
-print(kimp)
+
+print(kimp_bithumb)
+print(kimp_upbit)
