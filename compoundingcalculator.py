7da97b315098a9c3366642e504f265a7dda31aec
jwoo99 <kownjinwoo@gmail.com>
2022-10-09 10:57:43 +0900

    복리 계산기 v1 -오류수정

---
+++ compoundingcalculator.py
@@ -0,0 +1,27 @@
+import math
+import inquirer
+
+start = int(input("초기금 : "))
+per = int(input("이율 : ")) / 100
+choose = input("일 / 월 / 년 : ")
+
+while choose != "일" or choose != "월" or choose != "년":
+    choose = input("일 / 월 / 년 : ")
+    if choose == '일':
+        date = int(input("일 수 :"))
+        break
+    elif choose == '월':
+        date = int(input("월 수 :"))
+        break
+    elif choose == '년':
+        date = int(input("년 수 :"))
+        break
+    else:
+        print("제대로 된 값을 입력해주세요.")
+        continue
+
+
+m = start * ((1 + per) ** date)
+
+
+print(round(m))
