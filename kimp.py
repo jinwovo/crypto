b36d0785f586e2402331129674f5c3eec88dc30c
jwoo99 <kownjinwoo@gmail.com>
2022-10-09 10:35:17 +0900

    복리 계산기 v1

--- kimp.py
+++ kimp.py
@@ -21,7 +21,7 @@ url = "http://finance.naver.com/marketindex/"
 res = req.urlopen(url)
 soup = BeautifulSoup(res, "html.parser")
 abc = soup.select_one("div.head_info > span.value").string
-price_usd_krw = int(re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z\s]", "", abc)) / 100
+price_usd_krw = int(re.sub(r"[^\uAC00-\uD7A30-9a-zA-Z\s]", "", abc)) / 100 # 1,400.00 > 1400 형변환 및 특수문자 처리
 
 
 price_btc_krw_usdt = price_usd_krw * price_btc_usdt # 바이낸스 BTC 가격 KRW로 변환
@@ -32,6 +32,7 @@ kimp_bithumb =((price_bithumb_btc - price_btc_krw_usdt) / price_btc_krw_usdt ) *
 #Upbit 김프
 kimp_upbit =((price_upbit_btc - price_btc_krw_usdt) / price_btc_krw_usdt ) * 100
 
-
+print("↓↓↓↓빗썸김프↓↓↓↓")
 print(kimp_bithumb)
+print("↓↓↓↓업비트김프↓↓↓↓")
 print(kimp_upbit)
